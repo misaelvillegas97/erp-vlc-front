36f0162dea88e84abb4b184ec3f7a86e67d96fd6
David Misael Villegas Sandoval <david.misa97@gmail.com>
2025-03-26 02:02:06 -0300

    Add city field and enhance Create Supplier form
    ### public/i18n/es.json
    - Added new translation key for "city".
    
    ### src/app/modules/admin/maintainers/suppliers/pages/create/create.component.ts
    - Included new imports for MatFormFieldModule, MatInput, and TranslocoPipe.
    - Updated the form group to include required validation for fields such as email, phone, and new "city" field.
    
    ### src/app/modules/admin/maintainers/suppliers/pages/create/create.component.html
    - Updated template to include material form-field inputs for "rut," "business name," "fantasy name," "email," "phone," "address," and newly added "city" field.
    - Added validation messages for required and format-specific errors.
    
    ### src/app/modules/admin/maintainers/suppliers/pages/list/list.component.html
    - Updated the router link for creating a new supplier to be more descriptive.
    - Adjusted table data binding to handle potential undefined supplier resources.

--- src/app/modules/admin/maintainers/suppliers/pages/create/create.component.ts
+++ src/app/modules/admin/maintainers/suppliers/pages/create/create.component.ts
@@ -1,36 +1,41 @@
 import { Component, inject }                                                    from '@angular/core';
-import { TranslocoDirective, TranslocoService }                                 from '@ngneat/transloco';
+import { TranslocoDirective, TranslocoPipe, TranslocoService }                  from '@ngneat/transloco';
 import { PageDetailHeaderComponent }                                            from '@shared/components/page-detail-header/page-detail-header.component';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router }                                                               from '@angular/router';
 import { Notyf }                                                                from 'notyf';
 import { SupplierTypeEnum }                                                     from '@modules/admin/maintainers/suppliers/domain/enums/supplier-type.enum';
 import { SupplierTaxCategoryEnum }                                              from '@modules/admin/maintainers/suppliers/domain/enums/supplier-tax-category.enum';
+import { MatFormFieldModule }                                                   from '@angular/material/form-field';
+import { MatInput }                                                             from '@angular/material/input';
 
 @Component({
     selector   : 'app-create',
-    imports    : [
+    imports: [
         TranslocoDirective,
         PageDetailHeaderComponent,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        TranslocoPipe,
+        MatInput
     ],
     templateUrl: './create.component.html'
 })
 export class CreateComponent {
     readonly #fb = inject(FormBuilder);
     form: FormGroup = this.#fb.group({
-        rut             : new FormControl<string>(undefined, [ Validators.required ]),
-        businessName    : new FormControl<string>(undefined, [ Validators.required ]),
-        fantasyName     : new FormControl<string>(undefined, [ Validators.required ]),
+        rut         : new FormControl<string>(undefined, [ Validators.required ]), //*
+        businessName: new FormControl<string>(undefined, [ Validators.required ]), //*
+        fantasyName : new FormControl<string>(undefined, [ Validators.required ]), //*
         type            : new FormControl<SupplierTypeEnum>(SupplierTypeEnum.JURIDICA, [ Validators.required ]),
         taxCategory     : new FormControl<SupplierTaxCategoryEnum>(SupplierTaxCategoryEnum.PRIMERA_CATEGORIA, [ Validators.required ]),
         siiCode         : new FormControl<string>(undefined),
         economicActivity: new FormControl<string>(undefined),
-        address         : new FormControl<string>(undefined),
+        address     : new FormControl<string>(undefined), //*
         commune         : new FormControl<string>(undefined),
-        city            : new FormControl<string>(undefined),
-        phone           : new FormControl<string>(undefined),
-        email           : new FormControl<string>(undefined),
+        city        : new FormControl<string>(undefined), //*
+        phone       : new FormControl<string>(undefined), //*
+        email       : new FormControl<string>(undefined, [ Validators.email, Validators.required ]), //*
         contactPerson   : new FormControl<string>(undefined),
         contactPhone    : new FormControl<string>(undefined),
         isActive        : new FormControl<boolean>(true),
