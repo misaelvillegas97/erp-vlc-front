97ecc3812256d2cf9ddcf33c7e09f9de977a641a
David Misael Villegas Sandoval <david.misa97@gmail.com>
2025-07-24 22:17:37 -0400

    Refactor icon usage to utilize svgIcon for consistent rendering across components

--- src/app/modules/admin/apps/scrumboard/services/board.socket.ts
+++ src/app/modules/admin/apps/scrumboard/services/board.socket.ts
@@ -1,20 +1,23 @@
-import { Socket as NgSocket, Socket } from 'ngx-socket-io';
-import { AuthService }                from '@core/auth/auth.service';
-import { Injectable }                 from '@angular/core';
+import { Socket }                             from 'ngx-socket-io';
+import { AuthService }                        from '@core/auth/auth.service';
+import { ApplicationRef, inject, Injectable } from '@angular/core';
 
 @Injectable({providedIn: 'root'})
 export class BoardSocket extends Socket {
-    constructor(
-        public socket: NgSocket,
-        private authService: AuthService
-    ) {
+
+    constructor() {
+        const authService = inject(AuthService);
+        const appRef = inject(ApplicationRef);
+        
         super({
             url    : 'localhost:5000/ws/board',
             options: {
                 autoConnect: false,
-                transports : [ 'websocket' ],
-                auth       : {token: authService.accessToken}
+                transports: [ 'websocket' ]
             }
-        });
+        }, appRef);
+
+        // Set authentication token
+        this.auth = {token: authService.accessToken};
     }
 }
